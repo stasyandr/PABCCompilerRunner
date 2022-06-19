377903b04257c3b74b958c16bc15bbf05a57dfb1
stasyandr <45917470+stasyandr@users.noreply.github.com>
2022-06-19 13:46:47 +0300

    Add main files

---
+++ CompileRunHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System;
+using PascalABCCompiler.Errors;
+
+namespace PasCompiler
+{
+    public class Helper {
+        public static string CreateTempPas(string code) {
+            var myfilename = Path.GetTempFileName();
+            myfilename = Path.ChangeExtension(myfilename, "pas");
+            StreamWriter sw = new StreamWriter(myfilename);
+            sw.Write(code);
+            return myfilename;
+        }
+        public static string EnhanceErrorMsg(Object err0)
+        {
+            var err = err0 as LocatedError;
+            var msg = err.ToString();
+            string res = "";
+            var ind1 = msg.IndexOf('(');
+            var ind2 = msg.IndexOf(')');
+            var pos = "";
+            if (ind1 > -1 && ind2 > -1) {
+                pos = msg.Substring(ind1, ind2 + 1 - ind1);
+            }
+            if (ind2 > -1 && ind2 < msg.Length) {
+                ind2 = msg.IndexOf(':', ind2);
+                if (ind2 > -1 && ind2 < msg.Length - 1)
+                    ind2 = msg.IndexOf(':', ind2 + 1);
+                res = msg.Substring(ind2 + 1, msg.Length - ind2-1).Trim(' ');
+                if (pos != "")
+                    res = pos + ": " + res;
+            }
+            if (err0 is SemanticError)
+            {
+                SemanticError semErr = err0 as SemanticError;
+                pos = "(" + semErr.Location.begin_line_num + "," + semErr.Location.begin_column_num + ")";
+                res = pos + ": " + msg;
+            }
+            return res;
+        }
+    }
+}
---
+++ PABCCompilerRunner.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using PascalABCCompiler.SyntaxTree;
+using PascalABCCompiler.Errors;
+using PascalABCCompiler;
+using PascalABCCompiler.TreeConverter;
+
+namespace PasCompiler
+{
+    class PABCCompilerRunner
+    {
+        static string Compile(Compiler c, string myfilename) {
+            var co = new CompilerOptions(myfilename, CompilerOptions.OutputType.ConsoleApplicaton);
+            co.UseDllForSystemUnits = false;
+            co.Debug = false;
+            co.ForDebugging = false;
+            return c.Compile(co);
+        }
+        static string RunProcess(string myexefilename) {
+            var outputstring = new StringBuilder();
+            var pabcnetcProcess = new System.Diagnostics.Process();
+            pabcnetcProcess.StartInfo.FileName = myexefilename;
+            pabcnetcProcess.StartInfo.UseShellExecute = false;
+            //pabcnetcProcess.StartInfo.CreateNoWindow = true;
+            //pabcnetcProcess.StartInfo.RedirectStandardInput = true;
+            //pabcnetcProcess.StartInfo.StandardOutputEncoding = System.Text.Encoding.UTF8;
+            pabcnetcProcess.StartInfo.RedirectStandardOutput = true;
+            pabcnetcProcess.EnableRaisingEvents = true;
+            var outputOverflow = false;
+            pabcnetcProcess.OutputDataReceived += (o, e) => {
+                if (e.Data != null) {
+                    if (!outputOverflow) {
+                        outputstring.Append(e.Data);
+                        if (outputstring.Length > 10000) {
+                            outputstring.Length = 10000;
+                            outputOverflow = true;
+                            outputstring.Append("...");
+                        }
+                        outputstring.AppendLine();
+                    }
+                }
+            };
+            pabcnetcProcess.Start();
+            pabcnetcProcess.BeginOutputReadLine();
+            pabcnetcProcess.WaitForExit(5000);
+            if (!pabcnetcProcess.HasExited) { // убить процесс если он работвет больше 5 секунд. Скорее всего он завис
+                pabcnetcProcess.Kill();
+                outputstring.AppendLine("Program completed. It worked more then 5 seconds and probably stuck");
+            }
+            return outputstring.ToString();
+        }
+        static void Main(string[] args)
+        {            
+            if (args.Length == 0) {
+                System.Console.WriteLine("Need param - name .pas-file");
+                Environment.Exit(0);
+            }
+            StringResourcesLanguage.LoadDefaultConfig();
+            StringResourcesLanguage.CurrentLanguageName = "English";
+            var c = new Compiler();
+            var myfilename = args[0];
+            var myexefilename = Compile(c, myfilename);
+
+            var output = "";
+            if (myexefilename == null) {
+                if (c.ErrorsList.Count > 0) {
+                    var err = c.ErrorsList[0];
+                    output = Helper.EnhanceErrorMsg(err) + '\n';
+                }
+            }
+            else
+                output = RunProcess(myexefilename);
+            System.Console.WriteLine(output);
+        }
+    }
+}
